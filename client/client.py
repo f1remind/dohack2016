85ff5ba9afeea7a26da31e393356a10439f8370c
Jan-Niklas Nowak <xyznamexyz@gmail.com>
2016-07-17 02:35:17 -0400

    The client expects a version number followed by a list of available games
    The client expects the server to follow the protocoll:
    	send version id
    	recieve user name
    	send game id list
    	receive game id

--- client/client.py
+++ client/client.py
@@ -1,30 +1,63 @@
 import socket
+import gui
 
 class client():
-	def run(self):
+	def join(self, game):
+		pass
+	def connect(self):
 		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		s.connect((self.ip, self.port))
+		s.setblocking(False)
+		s.settimeout(60)
 		try:
 			#Get server version, do something if it does not fit
-			server_version = s.recv(1024).decode()
-			#Send username
-			s.send((self.name + '\n').encode())
-			#Get game list
-			#recieve string of csv
-			games_available = s.recv(1024).decode().split(';')
-			print('[DEBUG] Games available:',*games_available)
-			#Send game ID
-			while True:
-				antwort = s.recv(1024)
-				print('[{}] {}'.format(self.ip, antwort.decode()))
-				n = input('Nachricht: ')
-				n += '\n'
-				s.send(n.encode())
-		finally:
+			server_version = int(s.recv(1024).decode())
+			if server_version != self.version:
+				print('[ERROR] Version missmatch')
+				s.close()
+				s = None
+			else:
+				#Send username
+				s.send((self.name + '\n').encode())
+		except ConnectionRefusedError:
 			s.close()
+			s = None
+		return s
+	def select_game(self, s):
+		try:
+			games = s.recv(1024).decode().split(';')
+			games_available = []
+			for game in self.games:
+				if game[0] in games:
+					games_available.append(game)
+			self.gui.setGame(games_available)
+			self.gui.display()
+		except ConnectionRefusedError:
+			s.close()
+		#schicke die id des gewaehlten spieles
+	def join_game(self, s):
+		#Erwarte WAIT, READY, ERROR
+		#Bei READY: YOURTURN/NOTYOURTURN
+		#Bei erfolgreichem zug: Zughistory,
+		#Ber wiederholtem versagen -1 und der gegner hat einen zug
+		pass 
+	def run(self):
+		s = self.connect()
+		self.select_game(s)
+		
+		#Send game ID
+		while True:
+			antwort = s.recv(1024)
+			print('[{}] {}'.format(self.ip, antwort.decode()))
+			n = input('Nachricht: ')
+			n += '\n'
+			s.send(n.encode())
+		s.close()
 	def __init__(self, port=4586, ip = 'localhost',username=''):
+		self.games = {0:['TicTacToe']}
 		self.port = port
 		self.ip = ip
+		self.gui = gui.Gui()
 		self.version = 201607161730
 		while username == '':
 			username = input('Please enter a valid username: ')
